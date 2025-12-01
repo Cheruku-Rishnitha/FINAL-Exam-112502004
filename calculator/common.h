6e337216e4b8931326a61dc85e3afd45217d92ad
Cheruku-Rishnitha <https://github.com/Cheruku-Rishnitha/test-2-Rishnitha-2025.git>
2025-12-01 16:23:37 +0530

    1

--- calculator/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#define APP_NAME "Calculator"
-#define VERSION "1.0"
-
-#endif
--- calculator/config.c
+++
@@ -1,5 +0,0 @@
-#include "config.h"
-
-void loadConfig(AppConfig *config) {
-    config->enableLogging = 1; // Always on for this demo
-}
